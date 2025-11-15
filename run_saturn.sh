c5901d70de19ec4f8384f0da8bf77fe808296567
18373174212 <1359261542@qq.com>
2025-11-15 11:39:55 +0800

    11/15

--- run_saturn.sh
+++ run_saturn.sh
@@ -3,7 +3,7 @@ cd ./src
 # python main.py --optim_obj EDP --population 100 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 # python main.py --optim_obj EDP --population 20 --epochs 20  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 # python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-# python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.44 --all_DNN
+# python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.44 --all_DNN
 # python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 # python main.py --optim_obj EDP --population 20 --epochs 200  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload vgg16 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 # python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Gemmini --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
@@ -66,33 +66,33 @@ cd ./src
 # python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper Random --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 # echo "Random-resnet50."
 # python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper Random --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-llama2_34B."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_34B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-llama2_13B."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_13B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-llama2_7B."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_7B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-retinanet."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-unet."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload unet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-bert."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-vgg16."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload vgg16 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-alexnet."
-python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-echo "BO-resnet50."
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper BO --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.44 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload vgg16 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload unet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_7B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_13B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
-python main.py --optim_obj EDP --population 20 --epochs 40  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_34B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-llama2_34B."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_34B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-llama2_13B."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_13B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-llama2_7B."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload llama2_7B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-retinanet."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-unet."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload unet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-bert."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-vgg16."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload vgg16 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-alexnet."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+# echo "BO-resnet50."
+# python main.py --optim_obj EDP --population 20 --epochs 50  --accelerator Simba --mapper BO --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload resnet50 --layer_id 0 --batch_size 1 --expanded_scope 0.44 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload alexnet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload vgg16 --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload bert --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload unet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload retinanet --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_7B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_13B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
+python main.py --optim_obj EDP --population 20 --epochs 100  --accelerator Simba --mapper Saturn --type arch --version v1 --workload llama2_34B --layer_id 0 --batch_size 1 --expanded_scope 0.2 --all_DNN
 cd ..
